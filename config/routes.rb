3e219ad2e990ecfe8adca6c30bdd6706efe33ac8
Jevon Hayter <jevonhayter@gmail.com>
2016-02-04 14:57:56 +0000

    Added Privacy page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :exercises
   resources :contacts
   get '/about' => 'pages#about'
+  get '/privacy' => 'pages#privacy'
   root 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
