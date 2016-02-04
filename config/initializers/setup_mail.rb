69a707160ca4257a981ae9cc55bfdde710d822e9
Jevon Hayter <jevonhayter@gmail.com>
2016-02-04 23:31:37 +0000

    Added action mailer for forgot password feature

---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,10 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  :address              =>  'smtp.sendgrid.net',
+  :port                 =>  '587',
+  :authentication       =>  :plain,
+  :user_name            =>  'app46994141@heroku.com',
+  :password             =>  'mbap0wbm7331',
+  :domain               =>  'heroku.com',
+  :enable_starttls_auto  =>  true
+}
