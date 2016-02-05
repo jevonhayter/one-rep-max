5da38f09555cb93e435fee5c0d74b411d248ae4d
Jevon Hayter <jevonhayter@gmail.com>
2016-02-05 02:20:00 +0000

    Added action mailer for forgot password feature

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -5,6 +5,6 @@ ActionMailer::Base.smtp_settings = {
   :authentication       =>  :plain,
   :user_name            =>  'app46994141@heroku.com',
   :password             =>  'mbap0wbm7331',
-  :domain               =>  'obscure-cove-47194.herokuapp.com',
+  :domain               =>  'herokuapp.com',
   :enable_starttls_auto  =>  true
 }
