f0e4bb417cdfd18a8a6144c377b2bf320c6141b2
Jevon Hayter <jevonhayter@gmail.com>
2016-02-10 05:48:22 +0000

    working

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   
    devise_for :users 
+   
   resources :users do
     resources :exercises
   end
+  
   resources :exercises
   resources :contacts
   get '/about' => 'pages#about'
